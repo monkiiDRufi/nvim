283528df8570730d8f0296f60aede06667b84b1f
monkiiDRufi <rubenmcampos97@gmail.com>
2024-10-15 21:18:00 +0200

    Add my own toggle term

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -8,6 +8,37 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- terminal
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
+ToggleTerm = {
+	direction = "horizontal",
+	_getToggleCmd = function(self)
+		if self.direction == "horizontal" then
+			return "split"
+		end
+		if self.direction == "vertical" then
+			return "vsplit"
+		end
+	end,
+	toggle = function(self)
+		if not self._tbufnr then
+			vim.cmd(self:_getToggleCmd() .. "|terminal")
+			self._tbufnr = vim.fn.bufnr("%")
+			self._open_terminal = true
+			return
+		end
+
+		if not self._open_terminal then
+			vim.cmd(self:_getToggleCmd() .. "|b" .. self._tbufnr)
+		else
+			vim.cmd("q")
+		end
+		self._open_terminal = not self._open_terminal
+	end,
+}
+
+vim.keymap.set("n", "<leader>t", function()
+	ToggleTerm:toggle()
+end, { desc = "[T]oggle Terminal" })
+
 -- movement
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })

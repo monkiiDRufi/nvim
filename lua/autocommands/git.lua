cad15e71d5f73a0e82b2594035f28332f997c694
monkiiDRufi <rubenmcampos97@gmail.com>
2026-02-14 20:20:16 +0100

    validations and log

--- lua/autocommands/git.lua
+++ lua/autocommands/git.lua
@@ -1,5 +1,13 @@
 vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+  local line_number = tonumber(vim.fn.line('.'))
+  if not line_number or line_number < 1 then
+    vim.notify('Invalid line number', vim.log.levels.ERROR)
+    return
+  end
   local filename = vim.api.nvim_buf_get_name(0)
+  if filename == '' then
+    vim.notify('Cannot get filename', vim.log.levels.ERROR)
+    return
+  end
   print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
 end, { desc = 'Print the git blame for the current line' })

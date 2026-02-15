cb5fb1eb370fafa301fa81352c5287d408142c86
monkiiDRufi <rubenmcampos97@gmail.com>
2026-02-15 10:12:46 +0100

    Add autocmd for Markdown file settings
    Add autocmd to enable line wrapping and set text width for Markdown files.

--- lua/config/autocmd/lsp.lua
+++ lua/config/autocmd/lsp.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*.md',
+    callback = function(event)
+        vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.opt_local.textwidth = 100
+    end,
+})
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("LspKeymaps", { clear = true }),
 	callback = function(event)
